76d73f534bc66b9433b8551197e53031251183d0
Omer <omersaraf.develop@gmail.com>
2020-12-01 02:16:57 +0200

    test

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, send_from_directory, jsonify, request, send_file
+from flask import Flask, render_template, send_from_directory, request, send_file
 
-from hat_service import put_hat_on_it
+# from hat_service import put_hat_on_it
 
 app = Flask(__name__, static_url_path='')
 
@@ -14,17 +14,16 @@ def send_js(path):
 def hello():
     return render_template("pages/upload.html")
 
-
-@app.route('/submit', methods=['POST', 'GET'])
-def submit():
-    if 'file' not in request.files:
-        return "Missing file", 400
-    file = request.files['file']
-    if not file:
-        return "Empty file", 400
-    result = put_hat_on_it(file)
-    return send_file(result, "image/png", attachment_filename="with_hat.png", as_attachment=True)
-    # return jsonify({"success": True})
+#
+# @app.route('/submit', methods=['POST', 'GET'])
+# def submit():
+#     if 'file' not in request.files:
+#         return "Missing file", 400
+#     file = request.files['file']
+#     if not file:
+#         return "Empty file", 400
+#     result = put_hat_on_it(file)
+#     return send_file(result, "image/png", attachment_filename="with_hat.png", as_attachment=True)
 
 
 if __name__ == '__main__':
